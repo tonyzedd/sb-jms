8c5ad6e68a66c5c4cfddd0d9f7b65e1bd6d76d78
tonyzedd <tonyzed@gmail.com>
2017-12-18 09:45:11 +0000

    create pom file

---
+++ src/main/java/uk/me/tonyz/sbjms/SbJmsApplication.java
@@ -0,0 +1,15 @@
+package uk.me.tonyz.sbjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SuppressWarnings("CheckStyle")
+@SpringBootApplication
+public class SbJmsApplication {
+    /*
+    * todo
+    */
+	public static void main(String[] args) {
+	    SpringApplication.run(SbJmsApplication.class, args);
+	}
+}
---
+++ src/test/java/uk/me/tonyz/sbjms/SbJmsApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.me.tonyz.sbjms;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SbJmsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
